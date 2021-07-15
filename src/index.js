a8f754ee1e3890bd1ba3eeb0796123e4fd822c08
ViktarYeltsou <78786714+ViktarYeltsou@users.noreply.github.com>
2021-07-15 23:57:22 +0300

    fix: return [] when empty args

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    return matrix.map((el, i) => i % 2 ? el.reverse(): el).flat();
+    return !matrix ? [] : matrix.map((el, i) => i % 2 ? el.reverse(): el).flat();
 }
